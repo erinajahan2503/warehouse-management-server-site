72d23760176bfe01fc04551e1d31be4d69cfbda7
erina jahan <erinajahan3216@gmail.com>
2022-05-06 11:39:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('run my car haunter')
+});
+app.listen(port, () => {
+    console.log('car haunter is running');
+})
